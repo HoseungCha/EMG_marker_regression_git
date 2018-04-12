782aaae0778040785b8f1507f19601d7d425720b
HoseungCha <hoseungcha@gmail.com>
2018-04-12 13:10:05 +0900

    Simplifying Code
    get rid of unnecessary codes

--- functions/read_names_of_file_in_folder.m
+++ functions/read_names_of_file_in_folder.m
@@ -12,16 +12,32 @@
 function [name,FilepathFolder] = read_names_of_file_in_folder(filepath,extension)
 path=pwd;
 cd(filepath)
-if nargin>1
-    list=dir(extension);
+if nargin==1
+    list = dir;
+    id_read_only_folder =1;
 else
-list = dir;
+    list=dir(extension);
+    id_read_only_file =1;
 end
 count=1;
 for i=1:length(list)
+    % skip unnecessary files
     if ( strcmp(list(i).name,'.')==1 || strcmp(list(i).name,'..')==1)
         continue;
     end
+    % if you read only folder, skip files
+    if exist('id_read_only_folder') %#ok<EXIST>
+        if list(i).bytes~=0
+            continue;
+        end
+    end
+    % if you read only files, skip folder
+    if exist('id_read_only_file') %#ok<EXIST>
+        if list(i).bytes==0
+            continue;
+        end
+    end
+    % name which has been read
     name{count,1} = list(i).name;
     FilepathFolder{count,1} = fullfile(filepath,list(i).name);
     count = count +1;
--- test.m
+++
@@ -1,5 +0,0 @@
-layers = [ ...
-    sequenceInputLayer(12)
-    lstmLayer(100)
-    fullyConnectedLayer(9)
-    regressionLayer]
