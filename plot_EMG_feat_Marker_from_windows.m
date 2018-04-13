ad6848bb86611ce8fc4c45fe633b77628196a03d
HoseungCha <hoseungcha@gmail.com>
2018-04-13 11:36:21 +0900

    start coding plot EMG feat marker from windows
    start coding plot EMG feat marker from windows

---
+++ plot_EMG_feat_Marker_from_windows.m
@@ -0,0 +1,18 @@
+%--------------------------------------------------------------------------
+% 1: DB_windows_extraion.m
+% 2: Marker_v_ext_from_windows.m 
+% 3: EMG_feat_ext_from_windows.m %%%%%current code%%%%%%%%%%%%%%
+% 4: plot_EMG_feat_Marker_from_windows.m %%%%%current code%%%%%%%%%%%%%%
+%--------------------------------------------------------------------------
+% developed by Ho-Seung Cha, Ph.D Student,
+% CONE Lab, Biomedical Engineering Dept. Hanyang University
+% under supervison of Prof. Chang-Hwan Im
+% All rights are reserved to the author and the laboratory
+% contact: hoseungcha@gmail.com
+%--------------------------------------------------------------------------
+clc; close all; clear ;
+addpath(genpath(fullfile(cd,'functions')));
+% path for processed data
+parentdir=fileparts(pwd); % parent path which has DB files
+name_folder = 'windows_ds_10Hz_ovsize_50_delay_0';
+path_DB = fullfile(parentdir,'DB','DB_processed',name_folder);
