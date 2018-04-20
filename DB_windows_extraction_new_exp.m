78e21cdc21b629eba1b71bf916298c1e2031c165
HoseungCha <hoseungcha@gmail.com>
2018-04-20 13:01:18 +0900

    code trimming
    code trimming

--- DB_windows_extraction_new_exp.m
+++ DB_windows_extraction_new_exp.m
@@ -23,19 +23,19 @@ path_parent=fileparts(cd); % parent path which has DB files
 % get function
 addpath(genpath(fullfile(cd,'functions'))); % add path for functions
 %% read file path of data from raw DB
-[Sname,Spath] = read_names_of_file_in_folder(fullfile(path_parent,'DB','DB_raw2'));
+[name_sub,path_sub] = read_names_of_file_in_folder(fullfile(path_parent,'DB','DB_raw2'));
 
 %% experiment information
-N_subject = length(Sname);
-N_trial = 20;
-N_marker = 28;
-N_emgpair = 3;
-Name_Trg = {"화남",1,1;"어금니깨물기",1,2;"비웃음(왼쪽)",1,3;"비웃음(오른쪽)",...
+n_sub = length(name_sub);
+n_trl = 20;
+n_marker = 28;
+n_emg_pair = 3;
+name_trl = {"화남",1,1;"어금니깨물기",1,2;"비웃음(왼쪽)",1,3;"비웃음(오른쪽)",...
     1,4;"눈 세게 감기",1,5;"두려움",1,6;"행복",1,7;"키스",2,1;"무표정",2,2;...
     "슬픔",2,3;"놀람",2,4};
-Name_FE = Name_Trg(:,1);
-N_FE = length(Name_FE);
-Idx_trg = cell2mat(Name_Trg(:,2:3));
+name_FE = name_trl(:,1);
+n_FE = length(name_FE);
+idx_trg = cell2mat(name_trl(:,2:3));
 %% set parameters
 p_emg.SR = 2048;
 p_cam.SR = 120;
@@ -47,9 +47,9 @@ p_emg.lc_matrix = [10,9;10,8;9,8]; % 왼쪽 전극 조합
 
 % set window size and overlap size
 overlap_size = 0;
-SR_down = 120;
-[p_cam.winsize,p_cam.wininc] = calculate_window(p_cam.SR,SR_down,overlap_size,1);
-[p_emg.winsize,p_emg.wininc] = calculate_window(p_emg.SR,SR_down,overlap_size,1);
+sr_down = 120;
+[p_cam.winsize,p_cam.wininc] = calculate_window(p_cam.SR,sr_down,overlap_size,1);
+[p_emg.winsize,p_emg.wininc] = calculate_window(p_emg.SR,sr_down,overlap_size,1);
 
 % Bandpassfilter Parameters
 p_emg.Fn = p_emg.SR/2;
@@ -66,26 +66,26 @@ p_emg.NOF_Freq = [58 62];
 % load(fullfile(cd,'code_EMG_trigger_extraction','EMG_trg'));
 
 % set saving folder for windows
-Folder_Ances = sprintf('windows_ds_%dHz_ovsize_%d_delay_%d',SR_down,overlap_size,p_cam.delay);
-path_ances = make_path_n_retrun_the_path(fullfile(path_parent,'DB',...
-    'DB_processed2'),Folder_Ances);
+name_folder4saving = sprintf('windows_ds_%dHz_ovsize_%d_delay_%d',sr_down,overlap_size,p_cam.delay);
+path_DB_process = make_path_n_retrun_the_path(fullfile(path_parent,'DB',...
+    'DB_processed2'),name_folder4saving);
 
 %% get windows from EMG and marker set with each subject and trials
 % for i_sub= 5
-for i_sub= 1 : N_subject
+for i_sub= 1 : n_sub
 %% get file path
-sub_name = Sname{i_sub}(5:7); % get subject names
+sub_name = name_sub{i_sub}(5:7); % get subject names
 % get path of csv
-[~,path_csv] = read_names_of_file_in_folder(Spath{i_sub},'*csv');
+[~,path_csv] = read_names_of_file_in_folder(path_sub{i_sub},'*csv');
 % get path of bdf
-[~,path_bdf] = read_names_of_file_in_folder(Spath{i_sub},'*bdf');
+[~,path_bdf] = read_names_of_file_in_folder(path_sub{i_sub},'*bdf');
 
-for i_trl = 1 : N_trial
+for i_trl = 1 : n_trl
 % for i_trl = 10
     %% read BDF
-    OUT = pop_biosig(path_bdf{i_trl});
+    out = pop_biosig(path_bdf{i_trl});
     %% get triggers of EMG corresponding to each subject and trial
-    tmp = cell2mat(permute(struct2cell(OUT.event),[3 1 2]));
+    tmp = cell2mat(permute(struct2cell(out.event),[3 1 2]));
     tmp(:,1) = tmp(:,1)-tmp(1,1);
     tmp(:,1) = tmp(:,1)/128;
     lat_trg_onset = tmp(1,2);
@@ -100,14 +100,14 @@ for i_trl = 1 : N_trial
     tmp_emg_trg = sortrows([idx_in_order,(1:length(idx_in_order))'],1); 
     idx_seq_FE = tmp_emg_trg(:,2); clear tmp_emg_trg tmp ;
     
-    for i_comb = 1 : N_emgpair  %pairs biploar configuration of electrodes on cheek
+    for i_comb = 1 : n_emg_pair  %pairs biploar configuration of electrodes on cheek
         %%  bipolar configuration
-        emg_bip.RZ= OUT.data(p_emg.rc_matrix(i_comb,1),:) - ...
-            OUT.data(p_emg.rc_matrix(i_comb,2),:);
-        emg_bip.RF= OUT.data(4,:) - OUT.data(5,:);
-        emg_bip.LF= OUT.data(6,:) - OUT.data(7,:);
-        emg_bip.LZ= OUT.data(p_emg.lc_matrix(i_comb,1),:) - ...
-            OUT.data(p_emg.lc_matrix(i_comb,2),:);
+        emg_bip.RZ= out.data(p_emg.rc_matrix(i_comb,1),:) - ...
+            out.data(p_emg.rc_matrix(i_comb,2),:);
+        emg_bip.RF= out.data(4,:) - out.data(5,:);
+        emg_bip.LF= out.data(6,:) - out.data(7,:);
+        emg_bip.LZ= out.data(p_emg.lc_matrix(i_comb,1),:) - ...
+            out.data(p_emg.lc_matrix(i_comb,2),:);
 
         %% get bipolar channel names
         p_emg.ch_name = fieldnames(emg_bip);
@@ -140,28 +140,28 @@ for i_trl = 1 : N_trial
 %         emg_win = emg_win(1:trg_w(27));
 
         %% save
-        fname = sprintf('sub_%03d_trl_%03d',i_sub,i_trl); % file name to save
-        path_temp = make_path_n_retrun_the_path(path_ances,...
+        name_file = sprintf('sub_%03d_trl_%03d',i_sub,i_trl); % file name to save
+        path_temp = make_path_n_retrun_the_path(path_DB_process,...
             sprintf('emg_pair_%d',i_comb)); % get folder for saving
-        save(fullfile(path_temp,fname),'emg_win','trg_w','idx_seq_FE');
+        save(fullfile(path_temp,name_file),'emg_win','trg_w','idx_seq_FE');
     end
-    disp(fname); % for check code processing
+    disp(name_file); % for check code processing
 
     %% marker CSV Read
 %     fname = [Spath{i_sub},'\',num2str(i_trl),'.csv'];
-    [Marker_Data ,Labels,Time,NMarkers,NFrames,FrameRate] = csv2mat(path_csv{i_trl});
+    [marker_raw ,~,~,n_marker,~,~] = csv2mat(path_csv{i_trl});
 
     %% 카메라 첫번째 샘플을 없애줌(가짜 데이터)
-    Marker_Data(1,:,:) = [];
+    marker_raw(1,:,:) = [];
     %% nose 마커 기준으로 뺴줌
-    nose_marker = permute(Marker_Data(:,2,:),[1 3 2]);
-    for i_marker = 1 : NMarkers
+    nose_marker = permute(marker_raw(:,2,:),[1 3 2]);
+    for i_marker = 1 : n_marker
 %     for i_marker = 1:
         %% get relative marker values 
         if i_marker == 2 % marker가 nose일 경우  빼주지 말고 그냥 넣어줌
             mark_nose = nose_marker;
         else % marker가 nose가 아닐 경우  빼줌
-            mark_nose = nose_marker - permute(Marker_Data(:,i_marker,:),[1 3 2]);
+            mark_nose = nose_marker - permute(marker_raw(:,i_marker,:),[1 3 2]);
         end
         [az,el,r] = cart2sph(mark_nose(:,1),mark_nose(:,2),mark_nose(:,3));
         mk_data = [mark_nose(:,1),mark_nose(:,2),mark_nose(:,3),az,el,r];
@@ -176,19 +176,19 @@ for i_trl = 1 : N_trial
             [mark_win,~] = getWindows(mk_cell{i_mktype},...
                 p_cam.winsize,p_cam.wininc,[],[],[]); % get windows
             %% save
-            path_temp = make_path_n_retrun_the_path(path_ances,...
+            path_temp = make_path_n_retrun_the_path(path_DB_process,...
                 sprintf('mark_%d',i_marker)); % set folder name
-            fname = sprintf('sub_%03d_trl_%03d_%s',...
+            name_file = sprintf('sub_%03d_trl_%03d_%s',...
                 i_sub,i_trl,name_mk{i_mktype}); % name for saving
-            save(fullfile(path_temp,fname),'mark_win','trg_w','idx_seq_FE');
+            save(fullfile(path_temp,name_file),'mark_win','trg_w','idx_seq_FE');
         end
     end
 end
 end
 
 %% to confim the DB was extracted well
-load(fullfile(path_ances,'emg_pair_1','sub_001_trl_001'));
-load(fullfile(path_ances,'mark_10','sub_001_trl_001_raw'));
+load(fullfile(path_DB_process,'emg_pair_1','sub_001_trl_001'));
+load(fullfile(path_DB_process,'mark_10','sub_001_trl_001_raw'));
 % simple feat extraction
 N_win = length(mark_win);
 emg_rms = zeros(N_win,4);
@@ -199,4 +199,4 @@ for i_win = 1 : length(mark_win)
 end
 plot(zscore(emg_rms(:,1)));hold on;plot(zscore(mark_median(:,1)))
 hold on;
-stem(trg_w,ones(N_FE,1))
+stem(trg_w,ones(n_FE,1))
